98729a117f83755e3150e6944ac80c265ece4e0c
Adarsh Gupta <40561662+adarshgupta0129@users.noreply.github.com>
2025-05-06 12:48:46 +0530

    Update index.js nothing new

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   })
   .catch((err) => {
     console.error('❌ MongoDB Connection Failed:', err.message);
-    process.exit(1);
+    process.exit(1); 
   });
 
 app.listen(PORT, ()=>{  
